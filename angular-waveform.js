ca13e51f88772e1bd0a3db84e050478ab0fb063d
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2013-10-24 00:15:43 +0800

    support innerColor and outerColor

--- angular-waveform.js
+++ angular-waveform.js
@@ -9,11 +9,15 @@ angular.module('ngWaveform', [])
           if(wave) {
             var _width = attrs.width || 960
               , _height = attrs.height || 100
+              , _innercolor = attrs.innercolor || '#000'
+              , _outercolor = attrs.outercolor || '#fff'
               , waveform = new Waveform({
                    container: element[0],
                    data: wave,
                    width: _width,
                    height: _height,
+                   innerColor: _innercolor,
+                   outerColor: _outercolor
                 });
           }
         });
