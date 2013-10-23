730290ddb232650bc46a3cedd7e81505426648d3
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2013-10-24 00:28:51 +0800

    using waveform.update in watch

--- angular-waveform.js
+++ angular-waveform.js
@@ -5,20 +5,20 @@ angular.module('ngWaveform', [])
   .directive('ngWaveform', function () {
     return {
       link: function (scope, element, attrs) {
+        var _width = attrs.width || 960
+        , _height = attrs.height || 100
+        , _innercolor = attrs.innercolor || '#000'
+        , _outercolor = attrs.outercolor || '#fff'
+        , waveform = new Waveform({
+            container: element[0],
+            width: _width,
+            height: _height,
+            innerColor: _innercolor,
+            outerColor: _outercolor
+          });
         scope.$watch(attrs.ngWaveform, function (wave) {
           if(wave) {
-            var _width = attrs.width || 960
-              , _height = attrs.height || 100
-              , _innercolor = attrs.innercolor || '#000'
-              , _outercolor = attrs.outercolor || '#fff'
-              , waveform = new Waveform({
-                   container: element[0],
-                   data: wave,
-                   width: _width,
-                   height: _height,
-                   innerColor: _innercolor,
-                   outerColor: _outercolor
-                });
+            waveform.update({data: wave});
           }
         });
       }
